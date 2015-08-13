1df8af1abc0ae15c34dfd2082327e0e944644d14
Dave Clements <dave@theukedge.com>
2015-08-13 07:19:23 -0400

    Remove media items from items available to link to

--- includes/add-post-statuses.php
+++ includes/add-post-statuses.php
@@ -1,9 +1,15 @@
 <?php
 
 function apsfal_post_status_filter( $query ) {
-    $query['post_status'] = 'any';
+  $types = $query['post_type'];
 
-    return $query;
+  if( ($key = array_search('attachment', $types) ) !== false ) {
+    unset($types[$key]);
+  }
+  $query['post_status'] = 'any';
+  $query['post_type'] = $types;
+
+  return $query;
 }
 
 add_filter( 'wp_link_query_args', 'apsfal_post_status_filter' );
